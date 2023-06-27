67ee15d0edc6821d3ec592c3ff08865a8d4c9348
Dương Võ Thiên Bảo <135791389+Afihu@users.noreply.github.com>
2023-06-27 15:33:06 +0700

    mqtt.py

--- mqtt.py
+++ mqtt.py
@@ -1,58 +1,88 @@
+print("MQTT with Adafruit IO")
 import time
 import random
 import sys
 from Adafruit_IO import MQTTClient
+import requests
 
-AIO_FEED_ID = "sensor"
 AIO_USERNAME = "Afihu"
 AIO_KEY = ""
 
+global_equation = "x1 + x2 + x3"
+
+def init_global_equation():
+    global global_equation
+    headers = {}
+    aio_url = "https://io.adafruit.com/api/v2/Afihu/feeds/sensor.equation"
+    x = requests.get(url=aio_url, headers=headers, verify=False)
+    data = x.json()
+    global_equation = data["last_value"]
+    print("Get lastest value:", global_equation)
+
+def modify_value(x1, x2, x3):
+    global  global_equation
+    print("Equation: ", global_equation)
+    result = eval(global_equation)
+    return result
 
 def connected(client):
-    print("Ket noi thanh cong ...")
-    client.subscribe(AIO_FEED_ID)
+    print("Server connected ...")
+    client.subscribe("button1")
+    client.subscribe("button2")
+    client.subscribe("equation")
 
 def subscribe(client , userdata , mid , granted_qos):
-    print("Subscribe thanh cong ...")
+    print("Subscribed!")
 
 def disconnected(client):
-    print("Ngat ket noi ...")
+    print("Disconnected from the server!")
     sys.exit (1)
 
 def message(client , feed_id , payload):
-    print("Nhan du lieu: " + payload)
+    print("Received: " + payload)
+    if(feed_id == "equation"):
+        global  global_equation
+        global_equation = payload
+        print(global_equation)
 
 client = MQTTClient(AIO_USERNAME , AIO_KEY)
 
-client.on_connect = connected
+client.on_connect = connected  #callback
 client.on_disconnect = disconnected
 client.on_message = message
 client.on_subscribe = subscribe
 
 client.connect()
 client.loop_background()
+init_global_equation()
 
 while True:
-    print("Updating...")
     usage = random.randint(0,100)
     if usage >= 80: 
+        time.sleep(20)
         temp = random.randint(70, 90)
         client.publish("sensor.sensor1", temp)
         client.publish("sensor.sensor2", usage)
-        client.publish("sensor.sensor3", random.randint(1400,1805))
-        time.sleep(15)
+        clock = random.randint(1400,1805)
+        client.publish("sensor.sensor3", clock)
+        print('Result:',modify_value(temp, usage, clock ))
+        
     elif (usage < 80) and (usage >= 60): 
+        time.sleep(15)
         temp = random.randint(40, 68)
         client.publish("sensor.sensor1", temp)
         client.publish("sensor.sensor2", usage)
-        client.publish("sensor.sensor3", random.randint(1000,1400))
-        time.sleep(8)
+        clock = random.randint(1000,1400)
+        client.publish("sensor.sensor3", clock)
+        print('Result:',modify_value(temp, usage, clock ))
+
     else: 
+        time.sleep(10)
         temp = random.randint(30, 40)
         client.publish("sensor.sensor1", temp)
         client.publish("sensor.sensor2", usage)
-        client.publish("sensor.sensor3", random.randint(300,1000))
-        time.sleep(5)
-    
-while True:
+        clock = random.randint(300,1000)
+        client.publish("sensor.sensor3", clock)
+        print('Result:',modify_value(temp, usage, clock ))
+        
     pass
